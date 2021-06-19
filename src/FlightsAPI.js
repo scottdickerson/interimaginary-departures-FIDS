9b3f3dc433e1796a0f52e2cac641739a2d89a7b7
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2021-06-19 14:16:42 -0600

    fix(heroku): allow the app to listen to heroku

--- src/FlightsAPI.js
+++ src/FlightsAPI.js
@@ -2,10 +2,16 @@ import { normalizeFlight } from './dataUtils'
 import moment from 'moment'
 
 export const fetchFlights = (minutesToSeparate) =>
-    fetch(`${process.env.REACT_APP_SERVER_API_URL}/flights`, {
-        method: 'get',
-        url: process.env.REACT_APP_SERVER_API_URL,
-    }).then((response) => {
+    fetch(
+        `${
+            process.env.REACT_APP_SERVER_API_URL || 'http://127.0.0.1:8080'
+        }/flights`,
+        {
+            method: 'get',
+            url:
+                process.env.REACT_APP_SERVER_API_URL || 'http://127.0.0.1:8080',
+        }
+    ).then((response) => {
         console.log('fetch response')
         return response.json().then((flights) =>
             flights.map((flight, index) => ({
